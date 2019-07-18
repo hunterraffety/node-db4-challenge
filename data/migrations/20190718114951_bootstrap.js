d893d6d562cecb5cd0c2d2749c2045c739b1b471
hsrsbd <hsraffety@gmail.com>
2019-07-18 12:23:46 -0700

    ran migration to create db. testing it out in sqlite studio.

--- data/migrations/20190718114951_bootstrap.js
+++ data/migrations/20190718114951_bootstrap.js
@@ -1,8 +1,36 @@
-
 exports.up = function(knex) {
-  
+  //
+  return knex.schema
+    .createTable('recipes', tbl => {
+      tbl.increments();
+
+      tbl
+        .string('recipe_name')
+        .notNullable()
+        .unique();
+      tbl.string('recipe_instructions').notNullable();
+    })
+    .createTable('ingredients', tbl => {
+      tbl.increments();
+
+      tbl
+        .integer('recipe_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('recipes')
+        .onDelete('RESTRICT')
+        .onUpdate('CASCADE');
+      tbl
+        .string('ingredient_name')
+        .notNullable()
+        .unique();
+      tbl.float('quantity').notNullable();
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+    .dropTableIfExists('recipes')
+    .dropTableIfExists('ingredients');
 };
